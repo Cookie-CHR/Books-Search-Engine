f75f22eed888f5b65e4defb3b3a307924a9dbe41
SerenaPassini <Serena.Passini@outlook.it>
2020-12-27 17:28:31 +0100

    inserimento ricerca con sinonimi
    (si può migliorare)

--- querying.py
+++ querying.py
@@ -3,29 +3,49 @@ from whoosh.fields import *
 from whoosh.qparser import QueryParser, MultifieldParser
 import os, os.path
 from whoosh import qparser
+from nltk.corpus import wordnet as wn
+
+import nltk
+#roba che serve per far funzionare i sinonimi in italiano
+nltk.download('wordnet')
+nltk.download('omw')
 
 ix = open_dir("indexdir")
 
 # Faccio digitare la query dall'utente
 userQuery = input("Inserire una parola o frase da cercare: ")
+#creo la lista dei sinonimi
+synonyms = [userQuery]
+
+for syn in wn.synsets(userQuery, lang = "ita"):
+    for l in syn.lemmas(lang = "ita"):
+        if l.name() != userQuery:
+            synonyms.append(l.name())
 
 searcher = ix.searcher()
-#parser = QueryParser("contentData", schema=ix.schema)
-#parser = qparser.QueryParser("contentData", schema=ix.schema, group=qparser.OrGroup)
 
 #implementa la ricerca anche per titolo, autore o genere
 parser = MultifieldParser(["contentData", "title", "author", "genre"], schema=ix.schema) 
-query = parser.parse(userQuery)
-results = searcher.search(query)
+
+for s in synonyms:
  
+    query = parser.parse(s)
+    results = searcher.search(query)
+    
+    if len(results) > 0:
+        print(s)
+        # Restituisco i primi topN risultati - se esistono
+        topN = int(3) # primi 3 sennò viene un casino
+        i=1
+        
+        for r in results:
+            if i > topN:
+                break
+            
+            print(i, str(r.score), r['title'],r['author'], r['genre'])
+            print(r['content'][:255]+"...\n\n")
+            i+=1
 
-# Restituisco i primi topN risultati - se esistono
-topN = int(10) # il 10 l'ho messo a caso
-i=1
 
-for r in results:
-    if i > topN:
-        break
-    print(i, str(r.score), r['title'],r['author'], r['genre'])
-    print(r['content'][:255]+"...\n\n")
-    i+=1
+
+
