1dfd3b172b9fd888cadb2395bb53be57966d5a0b
Cookie-CHR <pacchetto99@gmail.com>
2021-01-02 16:41:48 +0100

    Refactoring, estratti i prezzi

--- genericScrape.py
+++ genericScrape.py
@@ -1,131 +1,53 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
-from bs4 import BeautifulSoup
-import pandas as pd
-import re
-from datetime import datetime
+from scrapeStruct import book, MondadoriBook, PiemmeBook, RizzoliBook
+import string
 
 
-def replaced(str):
+def replaced(s):
     # sostituisce alcuni caratteri di una stringa con altri.
     # serve per il passaggio da titolo/autore a parte del link
-    str = str.replace(" ","-")
-    str = str.replace("à","a")
-    str = str.replace("è","e")
-    str = str.replace("é","e")
-    str = str.replace("ì","i")
-    str = str.replace("ò","o")
-    str = str.replace("ó","o")
-    str = str.replace("ö","o")
-    str = str.replace("ù","u")
-    str = str.replace("ð","d")
-    return str
+    s = s.translate(str.maketrans('', '', string.punctuation)) # tolgo la punteggiatura
+    
+    s = s.replace(" ","-")
+    s = s.replace("à","a")
+    s = s.replace("è","e")
+    s = s.replace("é","e")
+    s = s.replace("é","e")
+    s = s.replace("ì","i")
+    s = s.replace("ò","o")
+    s = s.replace("ó","o")
+    s = s.replace("ö","o")
+    s = s.replace("ù","u")
+    s = s.replace("ð","d")
+    
+    return s
 
 ## funzione che si occupa dell'output su file
-def fileWrite(title, author, genre, link, plot):
+def fileWrite(title, author, genre, link, price, plot):
     outF = open("scraping/"+replaced(title)+".txt", "w")
     outF.write(title+"\n")
     outF.write(author+"\n")
     outF.write(genre+"\n")
     outF.write(link+"\n")
+    outF.write(price+"\n")
     outF.write(plot)
     outF.close()
 
-## Funzione che fa il web cscraping di titoli, autori, generi e link
-#  separata dal resto per ragioni di leggibilità
-def soupScraping(driver, titles, authors, genres, links, g):
-    content = driver.page_source
-    soup = BeautifulSoup(content, 'html.parser')
 
-    ## Estraggo le info e le metto negli array appositi
-    # non c'è bisogno di differenziare con un if sul sito, perché se soup non trova la classe giusta non avvierà neanche il for
-    
-    #qlibri
-    for a in soup.findAll(class_='listItem'):
-        title=a.find('a',href=True, attrs={'class':'jr_listingTitle'})
-        author=a.find('div', attrs={'class':'fieldValue'})
-        
-        # piccolo controllo, se non interrompo il ciclo mi dà errore
-        if title is None or author is None:
-            break
-        
-        # Creo il link e salvo la formattazione del genere senza trattini o barre
-        link = "https://www.qlibri.it/"+replaced(g)+"/"+replaced(title.text.replace("-","%11"))+"/"
-        genre = g.replace("-"," ").replace("/"," ")
-        
-        titles.append(title.text)
-        authors.append(author.text)
-        genres.append(genre) 
-        links.append(link)         
-    #mondadorilibri 
-    for a in soup.findAll(class_='product-info'):
-        title= a.find('a', attrs={'href' : re.compile('https://www.librimondadori.it/libri/.*')})
-        author=a.find('a', attrs={'href' : re.compile('https://www.librimondadori.it/autore/.*')})
-        
-        if title is None or author is None:
-            break
-          
-        # Creo il link e salvo la formattazione del genere senza trattini o barre
-        link="https://www.librimondadori.it/libri/"+replaced(title.text)+"-"+replaced(author.text)+"/"
-        genre = g.replace("-"," ").replace("/"," ")
-        
-        titles.append(title.text)
-        authors.append(author.text)
-        genres.append(genre) 
-        links.append(link)
-            
-    # Ritorno gli array per aggiornarli
-    return titles, authors, genres, links
+# Options
+options = Options()
+# Specify custom geckodriver path
+service = Service('/usr/lib/firefox/geckodriver')
 
-def loadingBar(X, maxX):
-    # loading bar, per sapere quante trame ho scaricato in percentuale 
-          
-    if X > maxX:
-        X = maxX
-    if X < 0:
-        X = 0
-    percent = (int)((float)(X/maxX*100)) 
-    loadState = ("#"*percent)+(" "*(100-percent))
-    return str(percent) + "%	["+loadState+"]"
+driver = webdriver.Firefox(options=options, service=service)
 
-
-
-start_time = datetime.now()
-numPlot = 1
-
-
-driver = webdriver.Firefox('/usr/lib/firefox/')
-
-# Array delle pagine da raschiare
-# (La maggior parte del link è sempre uguale, ho solo memorizzato la parte saliente: 
-# in librimondadori cambia solo il genere, in qlibri genere e categoria)
-
-mGenres = ["gialli-e-thriller", "narrativa-contemporanea", "romanzi-storici-fantasy-e-fantascienza"] # generi di mondadorilibri
-qGenres = ["romanzi", "gialli", "racconti", "romanzi-storici", "fantasy", "fantascienza", "classici"] # generi di qlibri
-qCategories = ["straniera","italiana"]
-
-# per ognuno dei due siti...
-for site in ["qlibri", "mondadorilibri", ]:
-    ### Per cominciare, definisco/vuoto gli array in cui mettere i risultati
-    titles = []
-    authors = []
-    genres = []
-    links = []
-    plots = []
+# per ognuno dei siti...
+for b in [PiemmeBook(), RizzoliBook(), MondadoriBook()]:
     
     ### In base al sito, trovo i link alle pagine generiche con le liste di titoli e autori
-    genericLinks = []
-    if site == "qlibri":
-        for c in qCategories:
-            for g in qGenres:
-                ## seleziono il driver giusto
-                genericLinks.append("https://www.qlibri.it/recensioni/"+g+"-narrativa-"+c+"/") 
-    elif site == "mondadorilibri": 
-        for g in mGenres:
-            for pagenum in range(1,8): # numero di pagine
-                genericLinks.append("https://www.librimondadori.it/genere/"+g+"/page/"+str(pagenum)+"/")
-                print("https://www.librimondadori.it/genere/"+g+"/page/"+str(pagenum)+"/")
+    genericLinks = b.findGenerics()
     
     ### Titolo, autore, genere, link
     # per ogni link generico trovato...
@@ -133,58 +55,14 @@ for site in ["qlibri", "mondadorilibri", ]:
         driver.get(nextLink)
         ## Trovo il genere a cui mi sto riferendo
         g = nextLink.split("/")[4]
-        if site == "qlibri": # qlibri ha bisogno di ulteriore formattazione
-            splittedg= g.split("-")
-            g = splittedg[len(splittedg)-2]+"-"+splittedg[len(splittedg)-1]+"/" # prima la categoria, poi una barra... 
-            for i in range (len(splittedg)-2):
-                g=g+splittedg[i]+"-" #poi il genere
-            g = g[:-1] #tolgo l'ultimo trattino
-        ##Estraggo titolo, autore e link alla trama estesa (vedi funzione apposita)
-        titles, authors, genres, links = soupScraping(driver, titles, authors, genres, links, g)
-    
-    ### Trama
-    for plotLink in links:
-        driver.get(plotLink)    
-        content = driver.page_source
-        soup = BeautifulSoup(content, 'html.parser')
-        if site == "qlibri":
-            # Qlibri è più simpatico: ha direttamente una classe per la trama
-            p = soup.find('div', attrs={'class':'contentFulltext'})
-            plot = p.text[4:] # via quei brutti tab dall'inizio della trama!
-            if plot is None:
-                break
-        elif site == "mondadorilibri":
-            # Mondadorilibri, invece, definisce tutti i contenuti della pagina con la p di paragrafo.
-            # Devo trovare, tra tutti i p, quelli della trama.
-            # il primo è sempre il 17 (prima ci sono info varie), seguito da un numero variabile di altri paragrafi.
-            # la trama finisce quando il paragrafo successivo descrive il genere
-            n=17
-            plot=""
-            while n<30:
-                p = soup.find_all('p')[n]
-                if p is None or p.text.startswith("Genere: "): # Se la trama è nulla o se si è già passati al genere
-                    break;
-                else:
-                    plot=plot+p.text+" "
-                    n+=1
-            plot.strip('\n')
-            # piccolo controllo se la trama è nulla
-            if plot is None:
-                break
-        
-        # Una serie di print per dare un'idea della situazione
-        # (questo programma ci mette 25 minuti a girare, diamogli almeno una barra di caricamento!)
-        d = datetime.now()-start_time
-        
-        print("\nTrama n. "+str(numPlot)+"\tTempo passato= "+ str(d.seconds // 3600)+":"+str(d.seconds % 3600 // 60)+":"+str(d.seconds%60))
-        print("qlibri:		"+loadingBar(numPlot, max(len(titles), 663)))
-        print("mondadorilibri:	"+loadingBar(numPlot-663, min(len(titles), 245)))
-        numPlot += 1
 
-        plots.append(plot)
-                    
+        ##Estraggo titolo, autore e link alla trama estesa (vedi funzione apposita)
+        b.soupScrapeGeneral(driver, g)
+    b.soupScrapeDeep(driver, g)
 
     ### Output su file
-    for i in range (0, len(titles)):
-        fileWrite(titles[i], authors[i], genres[i], links[i], plots[i])
+    for i in range (0, len(b.titles)):
+        fileWrite(b.titles[i], b.authors[i], b.genres[i], b.links[i], b.prices[i], b.plots[i])
+
+
 
---
+++ scrapeStruct.py
@@ -0,0 +1,343 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import re
+import string
+
+def replaced(str):
+    # sostituisce alcuni caratteri di una stringa con altri.
+    # serve per il passaggio da titolo/autore a parte del link
+    str = str.replace(" ","-")
+    
+    str = str.replace("à","a")
+    str = str.replace("è","e")
+    str = str.replace("é","e")
+    str = str.replace("ì","i")
+    str = str.replace("ò","o")
+    str = str.replace("ó","o")
+    str = str.replace("ö","o")
+    str = str.replace("ù","u")
+    
+    str = str.replace("À","A")
+    str = str.replace("È","E")
+    str = str.replace("É","E")
+    str = str.replace("Ì","I")
+    str = str.replace("Ò","O")
+    str = str.replace("Ù","U")
+    
+    str = str.replace("ð","d")
+    str.strip('\n')
+    return str
+    
+def somethingWrong(boolA, A, boolB, B, boolC, C, boolD, D, boolE, E):
+    return bool((boolA and A is None) or (boolB and B is None) or (boolC and C is None) or (boolD and D is None) or (boolE and E is None))
+
+class book: 
+    ## Classe padre, generica
+
+    # Funzioni per ritornare i vari attributi: saranno definite nelle classi figlie
+    def findGenerics(self):
+        pass 
+        
+    def findTitle(self, soup):
+        pass
+    def findAuthor(self, soup):
+        pass
+    def findLink(self, soup):
+        pass
+    def findGenre(self, soup):
+        pass
+    def findPlot(self, soup):
+        pass
+    def findPrice(self, soup):
+        pass
+        
+    ### Queste piccole funzioni definiscono se eseguire o no lo scraping di una determinata categoria
+    #   Servono per sapere quali categorie estrarre in soupScrapeGeneral e quali in soupScrapeDeep 
+    def ifTitle(self, soup, titleBool):
+        if titleBool:
+            t = self.findTitle(soup)
+            if t is not None:
+                if not isinstance(t, str):
+                    t = t.text
+                return t
+    def ifAuthor(self, soup, authorBool):
+        if authorBool:
+            a = self.findAuthor(soup)
+            if a is not None:
+                if not isinstance(a, str):
+                    a = a.text
+                return a
+    def ifGenre(self, soup, genreBool):
+        if genreBool:
+            g = self.findGenre(soup)
+            if g is not None:
+                if not isinstance(g, str):
+                    g = g.text
+                return g
+    def ifPrice(self, soup, priceBool):
+        if priceBool:
+            p = self.findPrice(soup)
+            if p is not None:
+                if not isinstance(p, str):
+                    p = p.text
+                return p
+    def ifPlot(self, soup, plotBool):
+        if plotBool:
+            p = self.findPlot(soup)
+            if p is not None:
+                if not isinstance(p, str):
+                    p = p.text
+                return p
+        
+    def __init__(self):
+        ## inizializzo tutti gli array che mi servono   
+        self.genericClass = ""
+    
+        self.titles = []
+        self.authors = []
+        self.plots = []
+        self.prices = []
+        self.genres = []
+        self.links = []
+    
+    
+    def soupScrapeGeneral(self, driver, g, titleBool, authorBool, genreBool, priceBool, plotBool):
+        content = driver.page_source
+        soup = BeautifulSoup(content, 'html.parser')        
+        
+        ## Estraggo le info già deducibili dalla pagina principale
+        for soup in soup.find_all(class_= self.genericClass):
+            title = self.ifTitle(soup, titleBool)
+            author = self.ifAuthor(soup, authorBool)
+            genre = self.ifGenre(soup, genreBool)
+            price = self.ifPrice(soup, priceBool)
+            plot = self.ifPlot(soup, plotBool)
+            
+            # Creo il link (questo va fatto per forza nella pagina principale, un ifLink non avrebbe senso)
+            link = self.findLink(soup)
+            
+            ## Check che tutto sia sincronizzato
+            if not somethingWrong(titleBool, title, authorBool, author, genreBool, genre, priceBool, price, plotBool, plot):
+                if link is not None:
+                    # appendo i risultati ai loro array
+                    self.links.append(link)
+                    if(titleBool): self.titles.append(title)
+                    if(authorBool): self.authors.append(author)
+                    if(genreBool): self.genres.append(genre)
+                    if(priceBool): self.prices.append(price)
+                    if(plotBool): self.plots.append(plot)
+                
+            
+        
+    def soupScrapeDeep(self, driver, g, titleBool, authorBool, genreBool, priceBool, plotBool):
+        ## In differita, dalla pagina di ogni libro estraggo il resto
+        #  itererò su una copia dell'array dei link, perché quello ufficiale può subire modifiche nel corso dello scraping
+        plotLinks = self.links.copy()
+        for plotLink in plotLinks:
+            driver.get(plotLink)    
+            content = driver.page_source
+            soup = BeautifulSoup(content, 'html.parser')
+            
+            #Estraggo le info richieste, e solo quelle
+            title = self.ifTitle(soup, titleBool)
+            author = self.ifAuthor(soup, authorBool)
+            genre = self.ifGenre(soup, genreBool)
+            price = self.ifPrice(soup, priceBool)
+            plot = self.ifPlot(soup, plotBool)
+            
+            ## Check che tutto sia sincronizzato
+            if somethingWrong(titleBool, title, authorBool, author, genreBool, genre, priceBool, price, plotBool, plot):
+                # C'è un problema, trovo qual è l'index in cui si è verificato e quali sono gli array da modificare
+                highs = [self.links]
+                problematicIndex = None
+                if (titleBool): problematicIndex = len(self.titles)
+                else: highs.append(self.titles)
+                if (authorBool): problematicIndex = len(self.authors)
+                else: highs.append(self.authors)
+                if (genreBool): problematicIndex = len(self.genres)
+                else: highs.append(self.genres)
+                if (priceBool): problematicIndex = len(self.prices)
+                else: highs.append(self.prices)
+                if (plotBool): problematicIndex = len(self.plots)
+                else: highs.append(self.plots)
+                    
+                print("Errore, verrà tolto il libro di index ", problematicIndex, ",",self.titles[problematicIndex])
+                
+                # tolgo dagli array "grossi" il dato che crea problemi
+                for array in highs:
+                        array.pop(problematicIndex)
+                        
+                
+            else: # se tutto è ok, appendo i risultati ai loro array
+                if(titleBool): self.titles.append(title)
+                if(authorBool): self.authors.append(author)
+                if(genreBool): self.genres.append(genre)
+                if(priceBool): self.prices.append(price)
+                if(plotBool): self.plots.append(plot)
+            
+            print(len(self.plots), "/",len(self.titles), "	", self.titles[len(self.plots)-1])
+        print ("Finito il soupscrape!")
+class MondadoriBook(book):
+    def __init__(self):
+        super().__init__()
+        self.genericClass = 'product-info'
+    
+    def findGenerics(self):
+        genericLinks = []
+        for g in ["gialli-e-thriller", "narrativa-contemporanea", "romanzi-storici-fantasy-e-fantascienza", "biografie-e-memoir"]:
+            for pagenum in range(1,8): # numero di pagine
+                pagenum = str(pagenum)
+                genericLinks.append("https://www.librimondadori.it/genere/"+g+"/page/"+pagenum+"/")
+        return genericLinks
+    
+    def findTitle(self, soup):
+        return soup.find('a', attrs={'href' : re.compile('https://www.librimondadori.it/libri/.*')})
+    def findAuthor(self, soup):
+        return soup.find('a', attrs={'href' : re.compile('https://www.librimondadori.it/autore/.*')})
+    def findLink(self, soup):
+        return soup.find('a', attrs={'href' : re.compile('https://www.librimondadori.it/libri/.*')})['href']
+    def findGenre(self, soup):
+        for p in soup.find_all('p'):
+            if p.text.startswith("Genere: "):
+                return p.text[8:]
+        
+    def findPlot(self, soup):
+        n=17
+        pp=""
+        while n<30:
+            p = soup.find_all('p')[n]
+            if p is None or p.text.startswith("Genere: "): # Se la trama è nulla o se si è già passati al genere
+                break;
+            else:
+                pp=pp+p.text+" "
+                n+=1
+        pp.strip('\n')
+        
+        return pp
+    def findPrice(self, soup):
+        for p in soup.find_all('p'):
+            if p.text.startswith("Prezzo: "):
+                return p.text[8:]
+        return None
+        
+    def soupScrapeGeneral(self, driver, g):
+        super().soupScrapeGeneral(driver, g, True, True, False, False, False)
+            
+    def soupScrapeDeep(self, driver, g):
+        super().soupScrapeDeep(driver,g, False, False, True, True, True)
+
+
+
+
+class PiemmeBook(book):
+    def piemmeReplaced(self, t):
+        # Piemme, oltre agli altri criteri di replace, non ammette maiuscole e punteggiatura
+        t = t.translate(str.maketrans('', '', string.punctuation))
+        
+        t = replaced(t)
+        t = t.lower()
+        return t
+            
+    def __init__(self):
+        super().__init__()
+        self.genericClass = 'col-sm-3'
+    
+    def findGenerics(self):
+        genericLinks = []
+        for g in ["fiction-thriller", "fiction-varia", "fiction-storici","non-fiction-memoir", "non-fiction-varia"]:
+            for pagenum in range(1,11): # numero di pagine
+                pagenum = str(pagenum)
+                genericLinks.append("https://www.edizpiemme.it/catalogo/adulti-"+g+"/?page="+pagenum)
+        return genericLinks
+    
+    def findTitle(self, soup):
+        t = soup.find('div', attrs={'class': 'post-heading'})
+        if t is not None:
+            t = t.text[1:-1] # questioni di formattazione, tolgo un invio da inizio frase e uno dalla fine
+        return t
+    def findAuthor(self, soup):
+        return  soup.find('a', attrs={'href' : re.compile('/autori/.*'), 'title': re.compile('Vai alla scheda dell\'autore .*')})
+    def findLink(self, soup):
+        link = soup.find('a', attrs={'href' : re.compile('/libri/.*')})
+        if link is not None:
+            link = "https://www.edizpiemme.it"+link['href']
+        return link
+    def findGenre(self, soup):
+        g = ""
+        for gen in soup.find_all('a', href=True, attrs={'title': re.compile('Vai al catalogo libri')}):
+            g = g + gen.text + " "
+        return g
+    def findPlot(self, soup):
+        p = soup.find('p')
+        if p is not None:
+            p = p.text
+        return p
+    def findPrice(self, soup):
+        return soup.find('span', attrs={'class': ['pull-right']})
+        
+    def soupScrapeGeneral(self, driver, g):
+        super().soupScrapeGeneral(driver, g, True, False, False, False, False,)
+    def soupScrapeDeep(self, driver, g):
+        super().soupScrapeDeep(driver,g, False, True, True, True, True)
+               
+        
+class RizzoliBook(book):
+    def __init__(self):
+        super().__init__()
+        self.genericClass = 'slider-ndnp-obj'
+    
+    def rizzoliReplaced(self, t):
+        #Anche Rizzoli non vuole punteggiatura
+        # Piemme, oltre agli altri criteri di replace, non ammette maiuscole e punteggiatura
+        t = t.translate(str.maketrans('', '', string.punctuation))
+        t = t.replace("’","")
+        t = replaced(t)
+        return t
+        
+    def findGenerics(self):
+        genericLinks = []
+        for g in ["san-valentino-storia-d-amore", "il-mondo-visto-dagli-animali", "giornata-mondiale-dellambiente-libri", \
+        "nero-rizzoli", "halloween-24-libri", "bur-verdi", "gatti-blu", "libri-da-leggere-sotto-lombrellone-estate-2017", \
+        "libri-ragazzi-estate-2017", "oltre-il-confine", "libri-da-regalare", "universi-paralleli", "narrativa-italiana-news", \
+        "gialli-attraverso-litalia", "love-is-love", "merry-books-fiction"]:
+            genericLinks.append("https://www.rizzolilibri.it/percorsi/"+g+"/")
+        return genericLinks
+    
+    def findTitle(self, soup):
+        title = soup.find('p', attrs={'class' : 'mt_20', 'class':'blue', 'class':'bold'})
+        if title is not None:
+            return title.text
+    def findAuthor(self, soup):
+        author = soup.find('p', attrs={'class' : 'page-subtitle', 'class':'mt_5'})
+        if author is not None:
+            return author.text[6:-4]+"\n" # tolgo i tab a inizio e fine riga
+    def findLink(self, soup):
+        link = soup.find('a', href=True, attrs={'class':'slider-ndnp-image'})
+        if link is not None:        
+            return link['href']
+        return None
+    def findGenre(self, soup):
+        tempGen = None
+        for li in soup.find_all('li'):
+            if li.text.startswith("Marchio: "):
+                tempGen = li.text.replace("Marchio: ", "").replace("	", "")
+            if li.text.startswith("Collana: "):
+                return li.text.replace("Collana: ", "").replace("	", "")
+        return tempGen
+                
+    def findPlot(self, soup):
+        plot = soup.find( attrs={'class' : 'gray', 'class':'mt_20', 'class':'min500'})
+        if plot is not None:
+            return plot.text.replace("\n","")
+    def findPrice(self, soup):
+        for li in soup.find_all('li'):
+            if li.text.startswith("Prezzo: "):
+                return li.text+"\n" 
+        
+    def soupScrapeGeneral(self, driver, g):
+        super().soupScrapeGeneral(driver, g, True, False, False, False, False)
+            
+    def soupScrapeDeep(self, driver, g):
+        super().soupScrapeDeep(driver,g, False, True, True, True, True)
+
+
