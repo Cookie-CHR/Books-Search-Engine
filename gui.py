4113fa0c9d87a0df6da0228f900ffd82485a276b
Cookie-CHR <pacchetto99@gmail.com>
2021-02-09 00:27:44 +0100

    collegati guy e query

--- gui.py
+++ gui.py
@@ -6,21 +6,33 @@ Created on Wed Jan 27 22:15:05 2021
 """
 
 import PySimpleGUI as sg
+from querying import searchWord
 
 sg.theme('Dark Blue 3')   #Aggiunge colore alla finestra
 
 # Tutta la roba all'interno della finestra
 layout = [  [sg.Text('Benvenuto/a nel nostro Search Engine!')],
+            
             [sg.Text("Inserire una parola o frase da cercare: "), sg.InputText()],
-            [sg.Button('Search'), sg.Button('Cancel')] ]
+            
+            [sg.Text("In quale categoria cercare: "), sg.OptionMenu(('Tutte', 'Titolo', 'Autore', 'Genere', 'Trama'))],
+            [sg.Text("In quale sito cercare: "), sg.OptionMenu(('Tutti', 'LibriMondadori', 'Piemme', 'Rizzoli'))],
+            
+            [sg.Button('Search'), sg.Button('Cancel')],
+         ]
 
 # Creazione della finestra
-window = sg.Window('Search Engine', layout)
+window = sg.Window('Search Engine', layout, element_justification='c')
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # Se l'utente chiude la finestra oppure clicca "Cancel"
         break
-   # print('You entered ', values[0])
+    elif event == 'Search':
+        print("Hai cercato", values[0])
+        results = searchWord(values[0], values[1])
+        for r in results:
+            print(str(r.score), r['title'],r['author'], r['genre'])
+            print(r['content'][:255]+"...\n\n")
 
 window.close()
--- querying.py
+++ querying.py
@@ -6,7 +6,6 @@ from whoosh import qparser
 from nltk.corpus import wordnet as wn
 import nltk
 import enchant
-from gui import *
 
 #roba che serve per far funzionare i sinonimi in italiano
 nltk.download('wordnet')
@@ -14,46 +13,60 @@ nltk.download('omw')
 
 ix = open_dir("indexdir")
 
-# Faccio digitare la query dall'utente
-userQuery = input("Inserire una parola o frase da cercare: ")
-# d = enchant.Dict()
-# d.check(userQuery)
-#creo la lista dei sinonimi
-synonyms = [userQuery]
+def catTranslate(category):
+    # Uno switch per identificare le categorie in cui effettuare la ricerca.
+    # category è la categoria scelta dall'utente tramite l'apposito menu a tendina
+    switcher={
+        "Tutte":["contentData", "title", "author", "genre", "content"],
+        "Titolo":["title"],
+        "Autore":["author"],
+        "Genere":["genre"],
+        "Trama":["contentData", "content"]
+    }
+    return switcher.get(category)
 
-for syn in wn.synsets(userQuery, lang = "ita"):
-    for l in syn.lemmas(lang = "ita"):
-        if l.name() != userQuery:
-            synonyms.append(l.name())        
+def searchWord(userQuery, category):
+    # d = enchant.Dict()
+    # d.check(userQuery)
+    #creo la lista dei sinonimi
+    synonyms = [userQuery]
+    
+    # Query expansion: aggiungo anche i sinonimi alla query
+    for syn in wn.synsets(userQuery, lang = "ita"):
+        for l in syn.lemmas(lang = "ita"):
+            if l.name() != userQuery:
+                synonyms.append(l.name())        
 
-searcher = ix.searcher()
-#implementa la ricerca anche per titolo, autore o genere
-parser = MultifieldParser(["contentData", "title", "author", "genre", "content"], schema=ix.schema) 
+    searcher = ix.searcher()
+    #implementa la ricerca anche per titolo, autore o genere, a seconda della scelta dell'utente
+    parser = MultifieldParser(catTranslate(category), schema=ix.schema) 
 
-for syn in synonyms:
-    query = parser.parse(syn)
-    # # Try correcting the query
-    # with ix.searcher() as s:
-    #     corrected = s.correct_query(query, syn)
-    #     if corrected.query != query:
-    #         print("Did you mean:", corrected.string)
+    for syn in synonyms:
+        query = parser.parse(syn)
+        # # Try correcting the query
+        # with ix.searcher() as s:
+        #     corrected = s.correct_query(query, syn)
+        #     if corrected.query != query:
+        #         print("Did you mean:", corrected.string)
     
 
-    results = searcher.search(query)
-    
-    if len(results) > 0:
-        print(syn)
-        # Restituisco i primi topN risultati - se esistono
-        topN = int(5) # primi 3 sennò viene un casino
-        i=1
+        results = searcher.search(query)
         
-        for r in results:
-            if i > topN:
-                break
+        # Creo l'array dei risultati da restituire all'interfaccia
+        resultsTot = [];
+    
+        if len(results) > 0:
+            print(syn)
+            # Restituisco i primi topN risultati - se esistono
+            topN = int(5) # primi 3 sennò viene un casino
+            i=1
+        
+            for r in results:
+                if i > topN:
+                    break
             
-            print(i, str(r.score), r['title'],r['author'], r['genre'])
-            print(r['content'][:255]+"...\n\n")
-            i+=1
-
+                resultsTot.append(r) # aggiungo r ai risultati
+                i+=1
+    return resultsTot
 
 
